df1dde96a8e8f1c52c220a606cefdba8f002f4fb
umaumax <umauma7083@gmail.com>
2019-10-02 00:47:30 +0900

    Update icicle_tree example

--- icicle_tree/script.js
+++ icicle_tree/script.js
@@ -4,25 +4,41 @@ $(function() {
     d3.json(json_url).then(function(data) {
         var width = 640
         var height = 480
-        var partition = data => d3.partition()
-            .size([height, width])
-            .padding(1)
-            (d3.hierarchy(data)
-                .sum(d => d.value)
-                .sort((a, b) => b.height - a.height || b.value - a.value))
-        console.log(data)
-        console.log(data.children)
+        var sort_flag = false;
+        var partition;
+        if (sort_flag) {
+            partition = data => d3.partition()
+                .size([height, width])
+                .padding(1)
+                (d3.hierarchy(data)
+                    .sum(d => d.value)
+                    .sort((a, b) => b.height - a.height || b.value - a.value))
+        } else {
+            partition = data => d3.partition()
+                .size([height, width])
+                .padding(1)
+                (d3.hierarchy(data)
+                    .sum(d => d.value)
+                )
+        }
+        // var color_pattern = 'children';
+        var color_pattern = 'ratio';
         var children_color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
-        var ratio_color = d3.interpolate("green", "red");
+        var parent_ratio_color = d3.interpolate("green", "yellow");
+        var children_ratio_color = d3.interpolate("yellow", "red");
         var scale = 2.0;
         var format = d3.format(",d")
+        var font_size = 16.0;
+        var value_unit = '[ms]'; // or '%'
 
         const root = partition(data);
 
         var body = d3.select("body");
+        body.append("text")
+            .text(d => data.title);
         var svg = body.append("svg")
             .attr("viewBox", [0, 0, width, height])
-            .style("font", "10px sans-serif");
+            .style("font", font_size + "px sans-serif");
 
         const cell = svg
             .selectAll("g")
@@ -35,29 +51,40 @@ $(function() {
             .attr("height", d => d.x1 - d.x0)
             .attr("fill-opacity", 0.6)
             .attr("fill", d => {
-                if (!d.depth) return "#ccc";
-                if (d.depth < -20) {
+                if ('color' in d.data) {
+                    return d.data['color'];
+                }
+                var has_child = 'children' in d;
+                if (color_pattern == 'children') {
+                    if (!d.depth) return "#ccc";
                     while (d.depth > 1) d = d.parent;
                     return children_color(d.data.name);
-                } else {
+                } else if (color_pattern == 'ratio') {
                     var value = (d.x1 - d.x0) / height;
-                    return ratio_color(value * scale);
+                    var ratio = value;
+                    if (has_child) {
+                        return parent_ratio_color(ratio / scale);
+                    }
+                    return children_ratio_color(ratio * scale);
+                } else {
+                    return '#ccc'
                 }
             });
 
         const text = cell.filter(d => (d.x1 - d.x0) > 16).append("text")
-            .attr("x", 4)
-            .attr("y", 13);
-
+            .attr("x", font_size)
+            .attr("y", font_size)
+            .attr("dx", "0em")
+            .attr("dy", "0em")
         text.append("tspan")
-            .text(d => d.data.name);
-
+            .text(d => d.data.name)
         text.append("tspan")
+            .attr("x", font_size)
+            .attr("dy", "1em")
             .attr("fill-opacity", 0.7)
-            .text(d => ` ${format(d.value)}`);
-
-        cell.append("title")
-            .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
+            .text(d => d.value)
+        text.append("tspan")
+            .text(d => value_unit)
     }).catch(function(error) {
         console.log('json load error', error);
     });
